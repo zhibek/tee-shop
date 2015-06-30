504091421377a44cffcfb67bb7a7ab7dab70bd06
John Levermore <john.levermore@zhibek.com>
2015-06-30 15:09:49 +0200

    Rename "weather" module to "camelcase_weather" to standardise naming.

--- magento/app/code/local/CamelCase/Weather/Block/Weather.php
+++ magento/app/code/local/CamelCase/Weather/Block/Weather.php
@@ -7,7 +7,7 @@ class CamelCase_Weather_Block_Weather extends Mage_Core_Block_Template
     public function getTemp()
     {
         if (!$this->client) {
-            $this->client = Mage::getModel('weather/client');
+            $this->client = Mage::getModel('camelcase_weather/client');
         }
         
         return $this->client->getTemp();
@@ -16,7 +16,7 @@ class CamelCase_Weather_Block_Weather extends Mage_Core_Block_Template
     public function getConditions()
     {
         if (!$this->client) {
-            $this->client = Mage::getModel('weather/client');
+            $this->client = Mage::getModel('camelcase_weather/client');
         }
         
         return $this->client->getConditions();
