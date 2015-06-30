d7f59419d78885878e62c518d8398b522d8f241a
John Levermore <john.levermore@zhibek.com>
2015-06-30 13:51:49 +0200

    Populate "Weather" block with weather from external API. Create weather/client model to communicate with API. Create weather/weather block to pass information to weather block template.

---
+++ magento/app/code/local/CamelCase/Weather/Block/Weather.php
@@ -0,0 +1,25 @@
+<?php
+class CamelCase_Weather_Block_Weather extends Mage_Core_Block_Template
+{
+
+    private $client;
+
+    public function getTemp()
+    {
+        if (!$this->client) {
+            $this->client = Mage::getModel('weather/client');
+        }
+        
+        return $this->client->getTemp();
+    }
+
+    public function getConditions()
+    {
+        if (!$this->client) {
+            $this->client = Mage::getModel('weather/client');
+        }
+        
+        return $this->client->getConditions();
+    }
+
+}
---
+++ magento/app/code/local/CamelCase/Weather/Model/Client.php
@@ -0,0 +1,72 @@
+<?php
+class CamelCase_Weather_Model_Client
+{
+
+    const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=%s';
+
+    const KELVIN_CELSIUS_CONVERSION = 273.15;
+
+    private $city = 'Cairo';
+
+    private $data;
+
+    private function retrieveWeather()
+    {
+        $url = sprintf(self::API_URL, $this->city);
+        $raw = file_get_contents($url);
+        $this->data = json_decode($raw);
+    }
+
+    public function getData($key=null)
+    {
+        if (!$this->data) {
+            $this->retrieveWeather();
+        }
+
+        if ($key) {
+            if (property_exists($this->data, $key)) {
+                return $this->data->{$key};
+            } else {
+                return null;
+            }
+        }
+
+        return $this->data;
+    }
+
+    public function setCity($city)
+    {
+        $this->city = $city;
+        $this->data = null;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function getTemp()
+    {
+        $main = $this->getData('main');
+
+        $tempKelvin = $main->temp;
+        if (!$tempKelvin) {
+            return null;
+        }
+
+        $tempCelsius = $tempKelvin - self::KELVIN_CELSIUS_CONVERSION;
+        $tempFormatted = sprintf('%d°C', $tempCelsius);
+
+        return $tempFormatted;
+    }
+
+    public function getConditions()
+    {
+        $weather = $this->getData('weather');
+        
+        $conditions = $weather[0]->main;
+
+        return $conditions;
+    }
+
+}
