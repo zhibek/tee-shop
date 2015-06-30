b023780b6183406a78498c585b91e09cab0e8c66
John Levermore <john.levermore@zhibek.com>
2015-06-30 15:46:53 +0200

    Add config option to admin allowing city to be set for the weather module. Set the default city (in config) to be "Cairo". Add a getter to the weather/data helper allowing the config value to be retrieved.

--- magento/app/code/local/CamelCase/Weather/Helper/Data.php
+++ magento/app/code/local/CamelCase/Weather/Helper/Data.php
@@ -1,2 +1,10 @@
 <?php
-class CamelCase_Weather_Helper_Data extends Mage_Core_Helper_Abstract {}
+class CamelCase_Weather_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+    public function getConfigCity()
+    {
+        return Mage::getStoreConfig('general/camelcase_weather/city');
+    }
+
+}
