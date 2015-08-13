b35a19bc0981281564721367508848d70862e6e9
Adham Saad <adham.sa3d@gmail.com>
2015-08-13 16:56:52 +0200

    Engravable Task 1 - create new module

---
+++ magento/app/code/local/CamelCase/Engravable/sql/engravable_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+$this->startSetup();
+ 
+$this->addAttribute(
+    'catalog_product',
+    'is_engravable',
+    array(
+        'input'         => 'boolean',
+        'type'          => 'int',
+        'label'         => 'Is Engravable',
+        'required'      => true,
+        'sort_order'    => 100
+        )
+);
+ 
+$this->endSetup();
