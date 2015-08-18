01342a14c7582724d85667f4a5c19c7343fc32e6
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-08-18 16:32:26 +0300

    task 4

--- magento/app/code/local/CamelCase/Engravable/Model/Observer.php
+++ magento/app/code/local/CamelCase/Engravable/Model/Observer.php
@@ -5,6 +5,7 @@ class CamelCase_Engravable_Model_Observer
 {
     public function logNameAndDate($observer)
     {
+        
         $params = Mage::app()->getRequest()->getPost();
         if(!empty($params['engravable_name'])) {
             $date  = date('d-m-Y');
@@ -12,8 +13,11 @@ class CamelCase_Engravable_Model_Observer
             $quote = $observer->getEvent()->getQuote();
             $product = Mage::getSingleton('catalog/product')->load($params['product']);
             $quote_item  = $quote->getItemByProduct($product);
+            $character_price = Mage::getStoreConfig('engravable/default/character_price');
+            $newPrice  = $quote_item->getBasePrice() + strlen($params['engravable_name']) * $character_price;
             $quote_item->setData('engraved_name', $params['engravable_name']);
             $quote_item->setData('engraved_date', $date);
+            $quote_item->setOriginalCustomPrice($newPrice);
             $quote_item->save();
             Mage::log("This an engravable Ring With Name '{$params['engravable_name']}' and date {$date}");
         }
