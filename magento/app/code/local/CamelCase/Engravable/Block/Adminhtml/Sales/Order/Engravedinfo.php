bb346c70e1700de45f13f9644a2fba61158c3b75
Adham Saad <adham.sa3d@gmail.com>
2015-08-24 13:18:41 +0200

    TASK 5 - add engraved data into order details

--- magento/app/code/local/CamelCase/Engravable/Block/Adminhtml/Sales/Order/Engravedinfo.php
+++ magento/app/code/local/CamelCase/Engravable/Block/Adminhtml/Sales/Order/Engravedinfo.php
@@ -2,26 +2,19 @@
 
 class CamelCase_Engravable_Block_Adminhtml_Sales_Order_EngravedInfo extends Mage_Adminhtml_Block_Sales_Items_Abstract
 {
-    protected $item;
-    
-    public function setItem($item)
+    public function getEngravedName()
     {
-        $this->item = $item;
+        return $this->getItem()->getEngravableName();
     }
     
+    public function getEngravedDate()
+    {
+        return $this->getItem()->getEngravableDate();
+    }
+
     public function getItem()
     {
-        return $this->item;
+        return $this->getParentBlock()->getItem();
     }
-    
-    public function __construct(array $args = array())
-    {
-        parent::__construct($args);
-    }
-    
-    
-    public function __call($method, $args)
-    {
-        Mage::log($method);
-    }
+
 }
