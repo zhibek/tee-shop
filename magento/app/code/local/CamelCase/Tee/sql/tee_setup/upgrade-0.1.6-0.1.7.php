660cde2896e9322daae5fe035055073876b9c8ee
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-09-13 17:42:36 +0300

    [2.1] Create Test Product #2

---
+++ magento/app/code/local/CamelCase/Tee/sql/tee_setup/upgrade-0.1.6-0.1.7.php
@@ -0,0 +1,33 @@
+<?php
+
+try {
+    Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+    $product = Mage::getModel('catalog/product');
+    $product
+        ->setWebsiteIds(array(1))
+        ->setAttributeSetId(4) //ID of a attribute set named 'default'
+        ->setTypeId('simple') //product type
+        ->setCreatedAt(strtotime('now')) //product creation time
+        ->setSku('testsku17') //SKU
+        ->setName('Test T-Shirt') //product name
+        ->setWeight(8.00)
+        ->setStatus(1) //product status (1 - enabled, 2 - disabled)
+        ->setTaxClassId(4) //tax class (0 - none, 1 - default, 2 - taxable, 4 - shipping)
+        ->setVisibility(Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH) //catalog and search visibility
+        ->setPrice(11.22) //price in form 11.22
+        ->setCost(22.33) //price in form 11.22
+        ->setMetaTitle('test meta title')
+        ->setMetaKeyword('testproduct')
+        ->setMetaDescription('test meta description')
+        ->setDescription('This is a long description')
+        ->setShortDescription('This is a short description')
+        ->setStockData(array(
+                'is_in_stock' => 1, //Stock Availability
+                'qty' => 999 //qty
+            )
+        )
+        ->setCategoryIds(array(6)); //assign product to categories
+    $product->save();
+} catch (Exception $e) {
+    Mage::log($e->getMessage());
+}
