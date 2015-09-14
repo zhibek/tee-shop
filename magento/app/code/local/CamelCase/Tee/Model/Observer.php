ee049312211fed2cbd14bd119b8c03642df25b83
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-09-14 13:14:00 +0300

    [2.1] Set Payment Method #3

---
+++ magento/app/code/local/CamelCase/Tee/Model/Observer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @category   MagePsycho
+ * @package    MagePsycho_Paymentfilter
+ * @author     magepsycho@gmail.com
+ * @website    http://www.magepsycho.com
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+    */
+class CamelCase_Tee_Model_Observer {
+ 
+    public function paymentMethodIsActive($observer) {
+        
+        $event           = $observer->getEvent();
+        $method          = $event->getMethodInstance();
+        $result          = $event->getResult();
+        
+            if($method->getCode() == 'checkmo' ){
+                $result->isAvailable = true;
+            }else{
+                $result->isAvailable = false;
+            }
+    }
+ 
+}
