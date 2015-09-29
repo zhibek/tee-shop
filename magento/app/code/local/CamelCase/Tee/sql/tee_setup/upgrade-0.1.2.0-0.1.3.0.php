9b37eecb7bf6cacdea4d03f7840816bf4873cd6b
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-09-29 15:56:33 +0200

    Fix Theme Migration (styles problem)

--- magento/app/code/local/CamelCase/Tee/sql/tee_setup/upgrade-0.1.2.0-0.1.3.0.php
+++ magento/app/code/local/CamelCase/Tee/sql/tee_setup/upgrade-0.1.2.0-0.1.3.0.php
@@ -8,6 +8,8 @@ $installer->startSetup();
 
 $config = new Mage_Core_Model_Config();
 $config->saveConfig('design/package/name', "bare", 'default', 0);
+$config->saveConfig('design/theme/skin', "", 'default', 0);
+$config->saveConfig('design/theme/default', "default", 'default', 0);
 
 
 $installer->endSetup();
