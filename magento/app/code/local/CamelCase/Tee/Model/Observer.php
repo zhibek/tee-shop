32ac1188b8f5172d4dec7abd8e64181c645e9f2d
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-09-30 13:56:10 +0200

    [2.1] Set Payment Method #3

--- magento/app/code/local/CamelCase/Tee/Model/Observer.php
+++ magento/app/code/local/CamelCase/Tee/Model/Observer.php
@@ -1,17 +1,4 @@
 <?php
 class CamelCase_Tee_Model_Observer {
  
-    public function paymentMethodIsActive($observer) {
-        
-        $event           = $observer->getEvent();
-        $method          = $event->getMethodInstance();
-        $result          = $event->getResult();
-        
-            if($method->getCode() == 'checkmo' ){
-                $result->isAvailable = true;
-            }else{
-                $result->isAvailable = false;
-            }
-    }
- 
 }
