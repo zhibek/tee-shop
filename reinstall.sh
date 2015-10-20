f297b222410f2b089edc92bb13a291dad786586e
ahmedReda1 <se_ahmedreda@yahoo.com>
2015-10-20 16:01:03 +0200

    #18 enhancing performance of reinstall.sh (mush faster)

--- reinstall.sh
+++ reinstall.sh
@@ -2,6 +2,12 @@
 
 rm /vagrant/magento/app/etc/local.xml || true
 mysql -uroot -p0000 -e 'DROP DATABASE IF EXISTS `tee-shop`; CREATE DATABASE `tee-shop`;'
+
+#Note: we cleared cache here to make the process faster
+rm -rf /vagrant/magento/var/cache/*
+
 /vagrant/bin/install.sh install
+
+#Note: removing cache and reindexing to fix search issue
 rm -r /vagrant/magento/var/cache/*
 php magento/shell/indexer.php --reindexall
