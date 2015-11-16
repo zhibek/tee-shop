0097bcbdf84854628afc399e8ec44ef754745164
ahmedReda1 <se_ahmedreda@yahoo.com>
2015-11-16 11:49:44 +0200

    #24 adding search module & adding search config & override catalogSearch::prepareResults()

---
+++ magento/app/code/local/TeeShop/Search/Model/Resource/Fulltext.php
@@ -0,0 +1,69 @@
+<?php
+
+class TeeShop_Search_Model_Resource_Fulltext extends Mage_CatalogSearch_Model_Resource_Fulltext
+{
+
+    public function prepareResult($object, $queryText, $query)
+    {
+        Mage::log("Done", null, 'events.log');
+        $adapter = $this->_getWriteAdapter();
+        if (!$query->getIsProcessed()) {
+            $searchType = $object->getSearchType($query->getStoreId());
+
+            $preparedTerms = Mage::getResourceHelper('catalogsearch')
+                    ->prepareTerms($queryText, $query->getMaxQueryWords());
+
+            $bind = array();
+            $like = array();
+            $likeCond = '';
+            if ($searchType == Mage_CatalogSearch_Model_Fulltext::SEARCH_TYPE_LIKE || $searchType == Mage_CatalogSearch_Model_Fulltext::SEARCH_TYPE_COMBINE
+            ) {
+                $helper = Mage::getResourceHelper('core');
+                $words = Mage::helper('core/string')->splitWords($queryText, true, $query->getMaxQueryWords());
+                foreach ($words as $word) {
+                    $like[] = $helper->getCILike('s.data_index', $word, array('position' => 'any'));
+                }
+                // we changed OR -> AND to remove irrelevant Results at type "Like" search 
+                if ($like) {
+                    $likeCond = '(' . join(' AND ', $like) . ')';
+                }
+            }
+            $mainTableAlias = 's';
+            $fields = array(
+                'query_id' => new Zend_Db_Expr($query->getId()),
+                'product_id',
+            );
+            $select = $adapter->select()
+                    ->from(array($mainTableAlias => $this->getMainTable()), $fields)
+                    ->joinInner(array('e' => $this->getTable('catalog/product')), 'e.entity_id = s.product_id', array())
+                    ->where($mainTableAlias . '.store_id = ?', (int) $query->getStoreId());
+
+            if ($searchType == Mage_CatalogSearch_Model_Fulltext::SEARCH_TYPE_FULLTEXT || $searchType == Mage_CatalogSearch_Model_Fulltext::SEARCH_TYPE_COMBINE
+            ) {
+                $bind[':query'] = implode(' ', $preparedTerms[0]);
+                $where = Mage::getResourceHelper('catalogsearch')
+                        ->chooseFulltext($this->getMainTable(), $mainTableAlias, $select);
+            }
+
+            if ($likeCond != '' && $searchType == Mage_CatalogSearch_Model_Fulltext::SEARCH_TYPE_COMBINE) {
+                $where .= ($where ? ' OR ' : '') . $likeCond;
+            }
+            elseif ($likeCond != '' && $searchType == Mage_CatalogSearch_Model_Fulltext::SEARCH_TYPE_LIKE) {
+                $select->columns(array('relevance' => new Zend_Db_Expr(0)));
+                $where = $likeCond;
+            }
+
+            if ($where != '') {
+                $select->where($where);
+            }
+
+            $sql = $adapter->insertFromSelect($select, $this->getTable('catalogsearch/result'), array(), Varien_Db_Adapter_Interface::INSERT_ON_DUPLICATE);
+            $adapter->query($sql, $bind);
+
+            $query->setIsProcessed(1);
+        }
+
+        return $this;
+    }
+
+}
