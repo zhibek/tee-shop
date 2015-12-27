2821bd4c8bde8a8c9d32bbac3541095ec285b0fa
Mohamed Labib <lebaz20@gmail.com>
2015-12-27 14:38:39 +0200

    Update engrave event observer - Save engraved name and date in quote and order

--- magento/app/code/local/Teeshop/Engrave/Model/Observer.php
+++ magento/app/code/local/Teeshop/Engrave/Model/Observer.php
@@ -2,7 +2,7 @@
 
 class Teeshop_Engrave_Model_Observer
 {
-    public function listenCartAdd() {
+    public function addCart() {
  
         $product = Mage::getModel('catalog/product')
                         ->load(Mage::app()->getRequest()->getParam('product', 0));
@@ -17,6 +17,26 @@ class Teeshop_Engrave_Model_Observer
             if(array_key_exists('engraved_name' ,$requestParameters)){
                 $engravedName = $requestParameters['engraved_name'];
                 Mage::log("new_product_with_engrave: $engravedName ,on: ".date(/*$format =*/ DATE_ATOM), /*$level =*/Zend_Log::INFO);
+            }
+        }
+        
+    }
+    
+    public function setQuote($observer) {
+ 
+        $quoteItem = $observer->getQuoteItem();
+        $product = $observer->getProduct();
+               
+        if (!$product->getId()) {
+            return;
+        }
+        $isEngravable = (bool)$product->getIsEngravable();
+        if( $isEngravable === true){
+            $requestParameters = Mage::app()->getRequest()->getParams();
+            if(array_key_exists('engraved_name' ,$requestParameters)){
+                $engravedName = $requestParameters['engraved_name'];
+                $quoteItem->setEngravedName($engravedName);
+                $quoteItem->setEngravedDate(date(/*$format =*/ DATE_ATOM));
             }
         }
         
